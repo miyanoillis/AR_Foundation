1a971972c75ff1a61398f37c329f3c731f4814d6
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-11-24 11:58:01 +0800

    1.1
    tap

---
+++ ARFoundation_107051053/Assets/ARManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.XR.ARFoundation;
+using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARManager : MonoBehaviour
+{
+    [Header("obj")]
+    public GameObject obj;
+    [Header("AR")]
+    public ARRaycastManager arr;
+
+    private Vector2 mouse;
+    private List<ARRaycastHit> hit;
+
+    private void tap()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            mouse = Input.mousePosition;
+        }
+    }
+
+    private void Update()
+    {
+        tap()
+    }
+}
